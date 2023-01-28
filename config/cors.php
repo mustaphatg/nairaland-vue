c22ab608169aa77f02aa3400f2b3f2300016c509
Ibrahim Mustapha <mustophatg@gmail.com>
2023-01-28 18:13:10 +0100

    changed cors settings

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('API_ORIGIN'), 'https://nairaland1.netlify.app'],
+    'allowed_origins' => ['https://nairaland1.netlify.app'],
 
     'allowed_origins_patterns' => [],
 
