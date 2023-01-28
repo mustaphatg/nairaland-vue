c29f4b166e4f9804b46686c3eb2bc39cd8d3a964
Ibrahim Mustapha <mustophatg@gmail.com>
2023-01-28 17:35:46 +0100

    added mobile side navigation

--- resources/nuxt/components/SideMenu.vue
+++ resources/nuxt/components/SideMenu.vue
@@ -1,11 +1,50 @@
 <template>
 
 
-    <ul class="menu p-4 w-80 bg-base-100 text-base-content">
-        <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
-    </ul>
+    <div class="w-4/5 bg-white overflow-hidden">
+
+        <smooth-scrollbar>
+            <!-- general -->
+            <div tabindex="0" class="collapse collapse-open my-3 collapse-arrow  bg-base-100 ">
+                <div class="collapse-title text-xl font-medium">
+                    General
+                </div>
+                <div class="collapse-content"> 
+                <ul class="menu   bg-white shadow-md border  rounded-lg p-2">
+                        <li @click="closeDrawer()" v-for="(link, index) in general" :key="index"><NuxtLink :to=" '/general'+link.href" >{{ link.text }}</NuxtLink></li>
+                    </ul>
+                </div>
+            </div> 
+
+
+            <!-- Enteratainment -->
+            <div tabindex="0" class="collapse collapse-open my-3 collapse-arrow  bg-base-100">
+                <div class="collapse-title text-xl font-medium">
+                    Enteratainment
+                </div>
+                <div class="collapse-content"> 
+                    <ul class="menu bg-white shadow-md border  rounded-lg p-2">
+                        <li @click="closeDrawer()" v-for="(link, index) in entertainment" :key="index"><NuxtLink :to=" '/entertainment'+link.href" >{{ link.text }}</NuxtLink></li>
+                    </ul>
+                </div>
+            </div>
+
+
+            <!-- Science -->
+            <div tabindex="0" class="collapse collapse-open my-3 collapse-arrow  bg-base-100 ">
+                <div class="collapse-title text-xl font-medium">
+                    Science 
+                </div>
+                <div class="collapse-content"> 
+                    <ul class="menu  bg-white shadow-md border rounded-lg p-2">
+                        <li v-for="(link, index) in science" :key="index"><NuxtLink :to=" '/science'+link.href" >{{ link.text }}</NuxtLink></li>
+                    </ul>
+                </div>
+            </div>
+
+        </smooth-scrollbar>
+
+    </div>
 
 
 </template>
@@ -30,6 +69,14 @@ export default {
             science : section.science
         }
     },
+
+    methods: {
+
+        closeDrawer(){
+            var drawer = document.querySelector('#my-drawer')
+            drawer.checked = !drawer.checked
+        }
+    }
 
 }
 </script>
